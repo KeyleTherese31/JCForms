d3dfd522b184faba4bca8fd57beecaff3d3f6340
Keyle Diaz <140049370+KeyleTherese31@users.noreply.github.com>
2025-05-13 13:04:41 +0800

    frontend 1

---
+++ frontend/src/views/LoginPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Login</h1>
+    <form @submit.prevent="login">
+      <input v-model="username" placeholder="Username" required />
+      <input v-model="password" type="password" placeholder="Password" required />
+      <button type="submit">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    };
+  },
+</script>
---
+++ frontend/src/views/WelcomePage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+      <h1>Welcome to JCForms</h1>
+      <router-link to="/login">Go to Login</router-link>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'Welcome',
+  };
+  </script>
+  
